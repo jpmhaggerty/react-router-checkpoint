2f40dbb5c10b418de90d6700f4fda1147684aeb7
James Haggerty <jpm.haggerty@gmail.com>
2021-10-18 14:08:31 -0400

    Home page additions

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,14 +29,38 @@ function Home() {
         <ButtonAppBar title="My Profile Page" />
       </header>
       <main>
-        <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-          <Grid item xs={12}>
-            <Grid container justifyContent="center" spacing={2}>
-              {myProfile.newsArticles.map((item, value) => (
-                <Grid key={value} item>
-                  <Card>{item.title}<br></br>{item.article}</Card>
-                </Grid>
-              ))}
+        <Grid>
+          <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+            <Grid item xs={4}>
+              <Grid container justifyContent="center" spacing={2}>
+                {myProfile.contactProfiles.map((item, value) => (
+                  <Grid key={value} item>
+                    <Card>
+                      {item.firstName} {item.lastName}
+                      <br></br>
+                      Birthday: {item.birthday}
+                      <br></br>
+                      Photo Tag: {item.profileImage}
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+            <Grid item xs={10}>
+              <Grid container justifyContent="right" spacing={2}>
+                {myProfile.newsArticles.map((item, value) => (
+                  <Grid key={value} item>
+                    <Card>
+                      {item.title}
+                      <br></br>
+                      {item.article}
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
