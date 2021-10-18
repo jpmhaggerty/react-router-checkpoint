8bfbcfe88c0a840fdfc2f12ba6857d07267bf735
James Haggerty <jpm.haggerty@gmail.com>
2021-10-18 12:19:40 -0400

    About blurb

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,16 @@
 import "../App.css";
 import ButtonAppBar from "./ButtonAppBar.js";
+import Box from "@mui/material/Box";
 
 function About() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonAppBar title="About Us"/>
+        <ButtonAppBar title="About Us" />
+        <Box>
+          <h1>Add company info here</h1>
+        </Box>
       </header>
-      <main>
-
-      </main>
     </div>
   );
 }
